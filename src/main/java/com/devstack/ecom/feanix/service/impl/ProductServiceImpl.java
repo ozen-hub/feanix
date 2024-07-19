5e1b2aa7648f8d0c144a540356ffb8585f99cf21
ozen-hub <info.ozenhub@gmail.com>
2024-07-19 20:46:52 +0530

    init

--- src/main/java/com/devstack/ecom/feanix/service/impl/ProductServiceImpl.java
+++ src/main/java/com/devstack/ecom/feanix/service/impl/ProductServiceImpl.java
@@ -1,15 +1,36 @@
 package com.devstack.ecom.feanix.service.impl;
 
 import com.devstack.ecom.feanix.dto.request.RequestProductDto;
+import com.devstack.ecom.feanix.entity.Product;
+import com.devstack.ecom.feanix.repository.ProductRepository;
 import com.devstack.ecom.feanix.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.UUID;
+
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+    private final ProductRepository repository;
+
     @Override
     public void create(RequestProductDto requestProductDto) {
+        repository.save(toProduct(requestProductDto));
+    }
 
+    private Product toProduct(RequestProductDto dto){
+        if (dto==null)return null;
+
+        return Product.builder()
+                .productId(UUID.randomUUID().toString())
+                .description(dto.getDescription())
+                .FileResource(null)
+                .orderDetails(null)
+                .qtyOnHand(dto.getQtyOnHand())
+                .unitPrice(dto.getUnitPrice())
+                .build();
     }
 }
