655109d8be360f770f10449b9fffe12e585aa29c
ozen-hub <info.ozenhub@gmail.com>
2024-07-31 21:23:22 +0530

    init

--- src/main/java/com/devstack/ecom/feanix/security/ApplicationUserPermission.java
+++ src/main/java/com/devstack/ecom/feanix/security/ApplicationUserPermission.java
@@ -1,4 +1,23 @@
 package com.devstack.ecom.feanix.security;
 
-public class ApplicationUserPermission {
+import java.util.Set;
+
+public enum ApplicationUserPermission {
+    PRODUCT_READ("product:read"),
+    PRODUCT_WRITE("product:write"),
+    ORDER_WRITE("order:write"),
+    ORDER_READ("order:read"),
+    USER_ROLE_WRITE("userRole:write");
+
+    private final String permission;
+
+    ApplicationUserPermission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+
 }
--- src/main/java/com/devstack/ecom/feanix/security/ApplicationUserRole.java
+++ src/main/java/com/devstack/ecom/feanix/security/ApplicationUserRole.java
@@ -1,4 +1,43 @@
 package com.devstack.ecom.feanix.security;
 
-public class ApplicationUserRole {
+import com.google.common.collect.Sets;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public enum ApplicationUserRole {
+    ADMIN(Sets.newHashSet(
+            ApplicationUserPermission.PRODUCT_READ,
+            ApplicationUserPermission.PRODUCT_WRITE,
+            ApplicationUserPermission.USER_ROLE_WRITE,
+            ApplicationUserPermission.ORDER_WRITE,
+            ApplicationUserPermission.ORDER_READ
+    )),
+    USER(Sets.newHashSet(
+            ApplicationUserPermission.PRODUCT_WRITE,
+            ApplicationUserPermission.ORDER_WRITE,
+            ApplicationUserPermission.ORDER_READ
+    ));
+
+    private final Set<ApplicationUserPermission> applicationUserPermissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> applicationUserPermissions) {
+        this.applicationUserPermissions = applicationUserPermissions;
+    }
+
+
+    public Set<ApplicationUserPermission> getApplicationUserPermissions() {
+        return applicationUserPermissions;
+    }
+
+    public Set<SimpleGrantedAuthority> grantedAuthorities() {
+        Set<SimpleGrantedAuthority> permissions = getApplicationUserPermissions()
+                .stream().map(permission ->
+                        new SimpleGrantedAuthority(permission.getPermission())).collect(Collectors.toSet());
+        permissions.add(
+                new SimpleGrantedAuthority("ROLE_" + this.name())
+        );
+        return permissions;
+    }
 }
