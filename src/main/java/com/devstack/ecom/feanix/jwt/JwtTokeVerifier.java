fc786dc8cc3d78f23ade77906aee47fc087cd274
ozen-hub <info.ozenhub@gmail.com>
2024-08-01 21:50:38 +0530

    init

---
+++ src/main/java/com/devstack/ecom/feanix/jwt/JwtTokeVerifier.java
@@ -0,0 +1,67 @@
+package com.devstack.ecom.feanix.jwt;
+
+
+import com.google.common.base.Strings;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class JwtTokeVerifier extends OncePerRequestFilter {
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+
+    public JwtTokeVerifier(JwtConfig jwtConfig, SecretKey secretKey) {
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION); // Bearer tokentextheeeheee
+        if(Strings.isNullOrEmpty(authHeader) || !authHeader.startsWith(jwtConfig.getTokenPrefix())){
+            filterChain.doFilter(request,response);
+            return;
+        }
+        String token = authHeader.replace(jwtConfig.getTokenPrefix(), "");
+        try{
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(secretKey)
+                    .parseClaimsJws(token);
+            Claims body = claimsJws.getBody();
+            String username = body.getSubject();
+            var authorities = (List<Map<String,String>>)body.get("authorities");
+           Set<SimpleGrantedAuthority> simpleGrantedAuthorities =
+                   authorities.stream().map(m-> new SimpleGrantedAuthority(m.get("authority")))
+                   .collect(Collectors.toSet());
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    username,
+                    null,
+                    simpleGrantedAuthorities
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }catch (JwtException e){
+            throw new IllegalStateException(String.format("Token %s Cannot be trusted...", token));
+        }
+
+    }
+}
