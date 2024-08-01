1dd76c5e8d02d71e3b80491ef8c54e6be46c79e3
ozen-hub <info.ozenhub@gmail.com>
2024-08-01 21:30:23 +0530

    init

---
+++ src/main/java/com/devstack/ecom/feanix/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.devstack.ecom.feanix.jwt;
+
+import com.devstack.ecom.feanix.dto.request.RequestApplicationUserLoginDto;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+    private final JwtConfig jwtConfig;
+    private final SecretKey secretKey;
+
+    public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager, JwtConfig jwtConfig, SecretKey secretKey) {
+        this.authenticationManager = authenticationManager;
+        this.jwtConfig = jwtConfig;
+        this.secretKey = secretKey;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response)
+            throws AuthenticationException {
+        try{
+            RequestApplicationUserLoginDto requestApplicationUserLoginDto = new ObjectMapper().readValue(request.getInputStream(),
+                    RequestApplicationUserLoginDto.class);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    requestApplicationUserLoginDto.getUsername(),
+                    requestApplicationUserLoginDto.getPassword()
+            );
+            return authenticationManager.authenticate(authentication);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities",authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        java.sql.Date.valueOf(LocalDate.now()
+                                .plusDays(jwtConfig.getTokenExpirationAfterDays()))
+                )
+                .signWith(secretKey)
+                .compact();
+        //response.addHeader("Authorization",jwtConfig.getTokenPrefix()+token);
+        response.addHeader(HttpHeaders.AUTHORIZATION,jwtConfig.getTokenPrefix()+token);
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        super.unsuccessfulAuthentication(request, response, failed);
+    }
+}
